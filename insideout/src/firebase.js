c12f2b5b2fed2d11852e8e91a4e55813ff8b290e
johnmartinroque <johnmartinroque@gmail.com>
2025-07-14 19:40:26 +0800

    firebase.js

---
+++ insideout/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYeqUf3IEpFj6AwevXAftRh1EiJuP131M",
+  authDomain: "insideout-ef98d.firebaseapp.com",
+  projectId: "insideout-ef98d",
+  storageBucket: "insideout-ef98d.firebasestorage.app",
+  messagingSenderId: "321694758874",
+  appId: "1:321694758874:web:4c551e3e48635d173732e3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
